0842367842b0fd6cec7107895ce56aebbccd7a0b
Dimitry Ivanov <saaboteur2012@yandex.ru>
2021-03-26 10:47:37 +0300

    testing

--- src/renderTemplate.js
+++ src/renderTemplate.js
@@ -1,8 +1,7 @@
 import React from "react";
 import App from "./pages/App.js";
 import ReactDOMServer from 'react-dom/server';
-import data from "./data/data";
 
-window.renderTemplate = () => {
-    return ReactDOMServer.renderToString(<App data={data}/>);
+window.renderTemplate = (data, alias) => {
+    return ReactDOMServer.renderToString(<App data={{data, alias}}/>);
 };
