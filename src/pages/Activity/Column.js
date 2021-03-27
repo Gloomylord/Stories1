a08d8fff8b43b0d80751937077b98f543687368b
Dimitry Ivanov <saaboteur2012@yandex.ru>
2021-03-27 17:18:15 +0300

    test 6

--- src/pages/Activity/Column.js
+++ src/pages/Activity/Column.js
@@ -16,15 +16,15 @@ function Column({res, value}) {
     return (
         <div className="activity_column">
             <div className="column">
-                <img src={`/assets/images/${src[0]}.svg`}
+                <img src={`assets/images/${src[0]}.svg`}
                      className="dark"
                      title={`${value} commit`}
                      alt={`${value} commit`}/>
-                <img src={`/assets/images/1x/${src[1]}.png`}
-                     srcSet={`/assets/images/1x/${src[1]}.png 500w,
-                         /assets/images/2x/${src[1]}.png 700w,
-                         /assets/images/3x/${src[1]}.png 1200w,
-                         /assets/images/4x/${src[1]}.png 1700w,`}
+                <img src={`assets/images/1x/${src[1]}.png`}
+                     srcSet={`assets/images/1x/${src[1]}.png 500w,
+                         assets/images/2x/${src[1]}.png 700w,
+                         assets/images/3x/${src[1]}.png 1200w,
+                         assets/images/4x/${src[1]}.png 1700w,`}
                      title={`${value} commit`}
                      className="light light-column"
                      alt={`${value} commit`}/>
--- src/renderTemplate.js
+++ src/renderTemplate.js
@@ -2,7 +2,6 @@ import React from "react";
 import App from "./pages/App.js";
 import ReactDOMServer from 'react-dom/server';
 
-window.renderTemplate = (alias, data) => {
-    console.log(alias, data);
+window.renderTemplate = function (alias, data) {
     return ReactDOMServer.renderToString(<App data={{alias, data}}/>);
 };
