0109e30f86288128ee3c39c547913d303d6a6980
Dimitry Ivanov <saaboteur2012@yandex.ru>
2021-03-28 18:51:28 +0300

    test 11

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -9,11 +9,11 @@ function UserData({data, emoji, showText, imgSize}) {
             <div className={cn("user-data")}>
                 <div className="user-img" style={{width: imgSize + 'px', height: imgSize + "px"}}>
                     <img
-                        src={'/assets/images/1x/' + data.avatar} alt="avatar"
-                        srcSet={`/assets/images/1x/${data.avatar} 500w,
-                         /ssets/images/2x/${data.avatar} 900w,
-                         /assets/images/3x/${data.avatar} 1000w,
-                         /assets/images/4x/${data.avatar} 1400w,`}
+                        src={'assets/images/1x/' + data.avatar} alt="avatar"
+                        srcSet={`assets/images/1x/${data.avatar} 500w,
+                         assets/images/2x/${data.avatar} 900w,
+                         assets/images/3x/${data.avatar} 1000w,
+                         assets/images/4x/${data.avatar} 1400w,`}
                     />
                     {emoji &&
                     <div className="emoji">
--- src/pages/Activity/Column.js
+++ src/pages/Activity/Column.js
@@ -16,19 +16,19 @@ function Column({res, value}) {
     return (
         <div className="activity_column">
             <div className="column">
-                <img src={`/assets/images/${src[0]}.svg`}
-                     srcSet={`/assets/images/1x/${src[0]}.png 500w,
-                         /assets/images/2x/${src[0]}.png 700w,
-                         /assets/images/3x/${src[0]}.png 1200w,
-                         /assets/images/4x/${src[0]}.png 1700w,`}
+                <img src={`assets/images/${src[0]}.svg`}
+                     srcSet={`assets/images/1x/${src[0]}.png 500w,
+                         assets/images/2x/${src[0]}.png 700w,
+                         assets/images/3x/${src[0]}.png 1200w,
+                         assets/images/4x/${src[0]}.png 1700w,`}
                      className="dark"
                      title={`${value} commit`}
                      alt={`${value} commit`}/>
-                <img src={`/assets/images/1x/${src[1]}.png`}
-                     srcSet={`/assets/images/1x/${src[1]}.png 500w,
-                         /assets/images/2x/${src[1]}.png 700w,
-                         /assets/images/3x/${src[1]}.png 1200w,
-                         /assets/images/4x/${src[1]}.png 1700w,`}
+                <img src={`assets/images/1x/${src[1]}.png`}
+                     srcSet={`assets/images/1x/${src[1]}.png 500w,
+                         assets/images/2x/${src[1]}.png 700w,
+                         assets/images/3x/${src[1]}.png 1200w,
+                         assets/images/4x/${src[1]}.png 1700w,`}
                      title={`${value} commit`}
                      className="light light-column"
                      alt={`${value} commit`}/>
